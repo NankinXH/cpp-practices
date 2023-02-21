6a639b634b9425b01332bffd20194becf168e646
NankinXH <finn108039@gmail.com>
2023-02-21 23:38:07 +0800

    update: upgrade 4.8

---
+++ 4.8Literals/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  std::cout << true << '\n';  // true evaluates to 1
+  std::cout << !true << '\n'; // !true evaluates to 0
+
+  bool b{false};
+  std::cout << b << '\n';  // b is false, which evaluates to 0
+  std::cout << !b << '\n'; // !b is true, which evaluates to 1
+  return 0;
+}
