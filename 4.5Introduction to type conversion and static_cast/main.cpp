0535d097a98fe69ae935f3a56816695e496a51e5
NankinXH <finn108039@gmail.com>
2023-02-24 23:33:59 +0800

    update: 4.5 updated

---
+++ 4.5Introduction to type conversion and static_cast/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// The print function take a double parameter;
+void print(double a) 
+{
+  cout << a;
+}
+int main()
+{
+  int y {7};
+  // y is of type int;
+  print(y);
+  
+  return 0;
+}
